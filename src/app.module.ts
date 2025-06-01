c2d2c5ee2c85cbcb58113f3cd377e6ee61f18b63
phongnaphat <phongnaphat.ch@mail.wu.ac.th>
2025-06-01 14:18:51 +0700

    Refactor module imports and enhance entity definitions; replace CategoryModule with CategoriesModule, add Goal and Transaction entities, and update user entity formatting.

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { UsersModule } from './modules/users/users.module';
-import { CategoryModule } from './modules/category/category.module';
+import { CategoriesModule } from './modules/categories/categories.module';
 
 @Module({
   imports: [
@@ -20,7 +20,7 @@ import { CategoryModule } from './modules/category/category.module';
       inject: [ConfigService],
     }),
     UsersModule,
-    CategoryModule,
+    CategoriesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/entities/category.entity.ts
+++ src/entities/category.entity.ts
@@ -1,24 +1,23 @@
-import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
-import { User } from './user.entity';
-import { create } from 'axios';
- 
-@Entity({ name: 'category' })
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, CreateDateColumn, UpdateDateColumn } from "typeorm";
+import { User } from "./user.entity";
+
+@Entity()
 export class Category {
-  @PrimaryGeneratedColumn()
-  id: number;
+    @PrimaryGeneratedColumn()
+    id: number;
 
-  @Column()
-  name: string;
+    @Column()
+    name: string;
 
-  @Column({ type: 'enum', enum: ['income', 'expense'] })
-  type: 'income' | 'expense';
+    @Column({ type: 'enum', enum: ['income', 'expense'] })
+    type: 'income' | 'expense';
 
-  @ManyToOne(() => User, (user) => user.id, { nullable: true })
-  user: User;
+    @ManyToOne(() => User, (user) => user.id, { nullable: true })
+    user: User;
 
-  @CreateDateColumn()
-  createdAt: Date;
+    @CreateDateColumn()
+    createdAt: Date;
 
-  @UpdateDateColumn()
-  updatedAt: Date;
+    @UpdateDateColumn()
+    updatedAt: Date;
 }
