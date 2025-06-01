6a7ab2916cc450d170308c6ab4c76dbbab079139
phongnaphat <phongnaphat.ch@mail.wu.ac.th>
2025-06-01 11:13:11 +0700

    Add Category and Notification entity definitions

---
+++ src/entities/category.entity.ts
@@ -0,0 +1,24 @@
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from 'typeorm';
+import { User } from './user.entity';
+import { create } from 'axios';
+
+@Entity({ name: 'category' })
+export class Category {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column({ type: 'enum', enum: ['income', 'expense'] })
+  type: 'income' | 'expense';
+
+  @ManyToOne(() => User, (user) => user.id, { nullable: true })
+  user: User;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
---
+++ src/entities/notification.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from './user.entity';
+
+@Entity({ name: 'notification' })
+export class Notification {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({unique: true})
+  title: string;
+
+  @Column()
+  content: string;
+
+  @ManyToOne(() => User, (user) => user.id)
+  user: User;
+}
